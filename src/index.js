08190243054b0d51b2880a8b2c41ac9680e727e4
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-04-28 20:08:01 -0700

    retry webpack and babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//ES6 Class called App
+//Create a method for this class called renderTemplate()
+//Inside of this method, utilize ES6 template strings to build out an index.html structure ]
+
+// After (with dynamic interface)
+import { footer, header,lunch-generator } from '../modules';
+
+import './main.css';
---
+++ src/modules/header.js
@@ -0,0 +1,16 @@
+class Header {
+
+    loadHeader() {
+        // Creata a new instance
+        const userProfile = new UserProfile();
+
+        // Invoke the method (component)
+        userProfile.loadUserProfile();
+
+        // Output loading status
+        console.log('Header component is loaded...')
+    }
+
+}
+
+export { Header };
